8e86f455c65c0591a044294fcd84831b75383d5e
nipathachaiwong <nipathachaiwong@gmail.com>
2014-11-18 04:52:46 +0700

    first working tableview
    everything works in the tableview. data is hard coded in .

--- ThaiBuddyGuide2/ThaiBuddyGuide2/ViewController.swift
+++ ThaiBuddyGuide2/ThaiBuddyGuide2/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet weak var myTableView: UITableView!
+    var arrayOfItems : [Item] = [Item]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.setUpItems()
+        
+        self.myTableView.delegate = self
+        self.myTableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setUpItems()
+    {
+        var item1 = Item(name: "temple1", imageName: "wat.jpeg")
+        var item2 = Item(name: "temple2", imageName: "wat2.jpeg")
+        
+        arrayOfItems.append(item1)
+        arrayOfItems.append(item2)
+        
+    }
+    
+    
+        
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        {
+            return arrayOfItems.count
+    }
+        
+        // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+        // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+        
+        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+        {
+            let cell: CustomCell = tableView.dequeueReusableCellWithIdentifier("cell") as CustomCell
+            
+            if indexPath.row % 2 == 0
+            {
+                cell.backgroundColor = UIColor.purpleColor()
+            }
+            else
+            {
+                cell.backgroundColor = UIColor.orangeColor()
+            }
+            
+            let item = arrayOfItems[indexPath.row]
+            
+            cell.setCell(item.name, imageName: item.imageName)
+            
+            return cell
+            
+            
+    }
 }
+
 
