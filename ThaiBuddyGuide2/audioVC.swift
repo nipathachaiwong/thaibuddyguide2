943886f4f3ddc95b60846c09e29723722f6f6f85
nipathachaiwong <nipathachaiwong@gmail.com>
2014-11-20 00:22:31 +0700

    button clicked sound play ok

---
+++ ThaiBuddyGuide2/audioVC.swift
@@ -0,0 +1,51 @@
+//
+//  audioVC.swift
+//  ThaiBuddyGuide2
+//
+//  Created by Thai Buddy on 11/19/14.
+//  Copyright (c) 2014 Thai Buddy Guides. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class audioVC: UIViewController, AVAudioPlayerDelegate{
+ 
+    
+    var audioPlayer: AVAudioPlayer?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var SoundUrl:NSURL? = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("testfordirectionaduio", ofType: "mp3")!)
+        if (SoundUrl != nil) {
+            self.audioPlayer = AVAudioPlayer(contentsOfURL: SoundUrl!, error: nil)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func playButton(sender: AnyObject) {
+        if (self.audioPlayer != nil) {
+            self.audioPlayer?.play()
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
